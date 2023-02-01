2d693f2ac7993a792427797d218da67efea3c48c
Edwin-Paul <github.paul01@gmail.com>
2023-02-01 12:14:33 +0300

    A pipeline to load data from Spotify API to SQLite using Airflow

--- airflow/dags/spotify_etl.py
+++ airflow/dags/spotify_etl.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-import pandas as pd
+import pandas as pd 
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
@@ -7,48 +7,54 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-DATABASE_LOCATION = "sqlite:///recent_played.sqlite"
-USER_ID = ""
-TOKEN = ""
-
 
 def check_if_valid_data(df: pd.DataFrame) -> bool:
-
+    # Check if dataframe is empty
     if df.empty:
-        print("No songs downloaded.")
-        return False
+        print("No songs downloaded. Finishing execution")
+        return False 
 
+    # Primary Key Check
     if pd.Series(df['played_at']).is_unique:
         pass
     else:
-        raise Exception("Primary Key Violation")
+        raise Exception("Primary Key check is violated")
 
-    
+    # Check for nulls
     if df.isnull().values.any():
-        raise Exception("Null Values Found")
+        raise Exception("Null values found")
 
+    # Check that all timestamps are of yesterday's date
     #yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
     #yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
 
     #timestamps = df["timestamp"].tolist()
     #for timestamp in timestamps:
-    #    if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
-    #        raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
-    
-    #return True
+    #   if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+    #       raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
 
-if __name__ == "__main__":
+    return True
 
+
+def run_spotify_etl():
+    database_location = "sqlite:///recent_played.sqlite"
+    user_id = '' # Enter Your Spotify User ID
+    token = '' # Get the spotify token from https://developer.spotify.com/console/get-recently-played/
+
+      # Extract part of the ETL process
+ 
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
-        "Authorization" : "Bearer {token}".format(token=TOKEN)
+        "Authorization" : "Bearer {token}".format(token=token)
     }
-
+    
+    # Convert time to Unix timestamp in miliseconds      
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=90)
+    yesterday = today - datetime.timedelta(days=100)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
+    # Download all songs you've listened to "after yesterday", which means in the last 24 hours      
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
     data = r.json()
@@ -58,12 +64,14 @@ if __name__ == "__main__":
     played_at_list = []
     timestamps = []
 
+    # Extracting only the relevant bits of data from the json object      
     for song in data["items"]:
         song_names.append(song["track"]["name"])
         artist_names.append(song["track"]["album"]["artists"][0]["name"])
         played_at_list.append(song["played_at"])
         timestamps.append(song["played_at"][0:10])
-
+        
+    # Prepare a dictionary in order to turn it into a pandas dataframe below       
     song_dict = {
         "song_name" : song_names,
         "artist_name": artist_names,
@@ -72,12 +80,14 @@ if __name__ == "__main__":
     }
 
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
-    print(song_df)
-
+    
+    # Validate
     if check_if_valid_data(song_df):
-        print("Data Valid, Proceed to load stage")
+        print("Data valid, proceed to Load stage")
 
-    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    # Load
+
+    engine = sqlalchemy.create_engine(database_location)
     conn = sqlite3.connect('recent_played.sqlite')
     cursor = conn.cursor()
 
@@ -85,9 +95,9 @@ if __name__ == "__main__":
     CREATE TABLE IF NOT EXISTS recent_played(
         song_name VARCHAR(200),
         artist_name VARCHAR(200),
-        played_at  VARCHAR(200),
+        played_at VARCHAR(200),
         timestamp VARCHAR(200),
-        CONSTRAINT primary_key_constraint PRIMARY KEY  (played_at)
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
     )
     """
 
@@ -100,4 +110,4 @@ if __name__ == "__main__":
         print("Data already exists in the database")
 
     conn.close()
-    print("Database closed successfully")
+    print("Close database successfully")
